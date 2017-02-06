92d54732e7c723a20a4e5b959276054ce4e06112
hanhtd <hanhtrd@yahoo.com>
2017-02-06 16:40:48 +0700

    finish lecture 2-4, add 3 Questions

--- src/main/scala-2.12/weekTwo/Rational.scala
+++ src/main/scala-2.12/weekTwo/Rational.scala
@@ -4,6 +4,28 @@ package weekTwo
   * Created by htruongduc on 2/6/2017.
   */
 class Rational(x: Int, y: Int) {
+  //Q_1: Why we use def at here instead of val or var
   def numer = x
+
   def denom = y
+
+  def add(that: Rational) = new Rational(this.numer * that.denom + that.numer * this.denom, this.denom * that.denom)
+
+  //new Rational(this.numer * that.denom - that.numer * this.denom, this.denom * that.denom)
+  def sub(that: Rational) = add(that.neg)
+
+
+  def mul(that: Rational) = new Rational(x * that.numer, y * that.denom)
+
+  def neg() = new Rational(-numer, denom)
+
+  //Q_2: what is difference between equals and == in scala
+  override def equals(obj: scala.Any): Boolean = if (obj != null && obj.isInstanceOf[Rational]) {
+    val objRational = obj.asInstanceOf[Rational]
+    this.numer * objRational.denom == this.denom * objRational.numer
+  } else false
+
+  override def toString: String = numer + "/" + denom
 }
+
+//Q_3: what is difference between function and method in scala
--- src/main/scala-2.12/weekTwo/week2-4.sc
+++ src/main/scala-2.12/weekTwo/week2-4.sc
@@ -3,3 +3,22 @@ import weekTwo.Rational
 val x = new Rational(1, 2)
 x.numer
 x.denom
+
+def addRational(r: Rational, s: Rational): Rational = new Rational(r.numer * s.denom + s.numer * r.denom, s.denom * r.denom)
+def makeString(r: Rational) = r.numer + "/" + r.denom
+
+makeString(addRational(new Rational(1, 2), new Rational(2, 3)))
+
+val y = new Rational(4, 5)
+val z = new Rational(4, 8)
+x add y
+
+x == y
+x == z
+z.neg
+
+val a = new Rational(1, 3)
+val b = new Rational(5, 7)
+val c = new Rational(3, 2)
+
+a sub b sub c
