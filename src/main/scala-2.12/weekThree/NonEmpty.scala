18aa9e82b8917d6bd672c160b5e440ee379a66ea
Truong Duc Hanh <htruongduc@pentalog.fr>
2017-02-15 18:04:08 +0700

    add todo for week 3

--- src/main/scala-2.12/weekThree/NonEmpty.scala
+++ src/main/scala-2.12/weekThree/NonEmpty.scala
@@ -3,6 +3,9 @@ package weekThree
 /**
   * Created by htruongduc on 2/15/2017.
   */
+//TODO: 1. Read + practice recursive algorithm with datastructure&algorithm course and discrete math
+//TODO: 2. How to thinking in functional
+//TODO: 3. Review to find out how to read + practice in week 1 && week 2
 class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
   def incl(x: Int): IntSet = if (x < elem) new NonEmpty(elem, left incl x, right) else if (x > elem) new NonEmpty(elem, left, right incl x) else this
 
