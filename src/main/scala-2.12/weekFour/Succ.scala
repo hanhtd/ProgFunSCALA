09d439d4549457fe6be97e591b512310dbbd9862
Truong Duc Hanh <htruongduc@pentalog.fr>
2017-06-22 18:00:21 +0700

    [week.4]add first version of Nat implemetation

--- src/main/scala-2.12/weekFour/Succ.scala
+++ src/main/scala-2.12/weekFour/Succ.scala
@@ -3,6 +3,18 @@ package weekFour
 /**
   * Created by htruongduc on 6/22/2017.
   */
-class Succ {
+abstract class Nat {
+  def isZero: Boolean
+  def predecessor: Nat
+  def successor: Nat
+  def + (that: Nat): Nat
+  def - (that: Nat): Nat
+}
+
+class Succ(n: Nat) extends Nat{
+
+}
+
+object Zero extends Nat {
 
 }
