b7496297ee8c517ebbcca59a2cc11f8a09b1bb13
Truong Duc Hanh <htruongduc@pentalog.fr>
2017-06-23 18:05:09 +0700

    update code for week4_1_App

--- src/main/scala-2.12/weekFour/week4_1_App.scala
+++ src/main/scala-2.12/weekFour/week4_1_App.scala
@@ -4,5 +4,5 @@ package weekFour
   * Created by htruongduc on 6/23/2017.
   */
 object week4_1_App extends App {
-  println(new Succ(Zero).successor.successor.toString())
+  println(new Succ(Zero).successor.successor.toInt)
 }
